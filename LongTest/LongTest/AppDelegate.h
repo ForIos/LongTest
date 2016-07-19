34f2cb43f656d953732477929d169ebfec687d1e
LL <894143788@qq.com>
2016-07-19 14:08:00 +0800

    LongTest
    LongTest

---
+++ LongTest/LongTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LongTest
+//
+//  Created by lcc on 16/7/19.
+//  Copyright © 2016年 ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LongTest/LongTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LongTest
+//
+//  Created by lcc on 16/7/19.
+//  Copyright © 2016年 ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
